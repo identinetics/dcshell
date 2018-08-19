bf8aae67d087b2ff926be04892ba3af321bdce71
rhoerbe <rainer@hoerbe.at>
2018-08-19 20:23:08 +0200

    test_build1.py working partially

--- test/testin/build/test_build1.py
+++ test/testin/build/test_build1.py
@@ -1,5 +1,7 @@
+import os.path
 import pytest
 import subprocess
 
 def test_build1():
-    subprocess.call('../../build')
+    script = os.path.abspath('build')
+    subprocess.call([script, '-D', 'test', '-f', 'testenv/dc_build1.yaml', '-v'])
