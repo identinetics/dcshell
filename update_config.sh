79ace2b3c2566fd4c9b046bb56a6b4d5a3ed4ecd
rhoerbe <rainer@hoerbe.at>
2018-12-18 19:40:19 +0100

    update_config:  explicit target to avoid empty output on missing input

--- update_config.sh
+++ update_config.sh
@@ -2,4 +2,6 @@
 
 # Replace docker registry owner 'local' by $DOCKER_REGISTRY_USER
 
-perl -pe "s/^(\s+)image:\s+local/\$1image: $DOCKER_REGISTRY_USER/" $1
+if [[ -e "$1" ]]; then
+    perl -pe "s/^(\s+)image:\s+local/\$1image: $DOCKER_REGISTRY_USER/" $1 > $2
+fi
