0f478f6a36a9b88b66e5807d49f605f0bf632236
rhoerbe <rainer@hoerbe.at>
2019-05-15 20:40:06 +0200

    add explicit DOCKER_REPO_HOST (+refactor build, add tests)

--- dcshell_lib.sh
+++ dcshell_lib.sh
@@ -2,24 +2,9 @@
 PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
 check_python3() {
-    python3 -c exit >/dev/null 2>&1
-    if (( $? > 0 )); then
+    if ! command -v python3 &>/dev/null; then
         echo "python3 not found in path. Cannot proceed."
         exit 5
-    fi
-}
-
-
-get_container_status() {
-    if [[ ! "$CONTAINERNAME" ]]; then
-        echo 'CONTAINERNAME must not be empty'
-        exit 1
-    elif [[ "$($sudo docker ps -f name=$CONTAINERNAME |egrep -v ^CONTAINER)" ]]; then
-        return 0   # running
-    elif [[ "$($sudo docker ps -a -f name=$CONTAINERNAME|egrep -v ^CONTAINER)" ]]; then
-        return 1   # stopped
-    else
-        return 2   # not found
     fi
 }
 
@@ -38,11 +23,8 @@ load_compose_yaml_config() {
     # config.py will create 'export X=Y' statements on stdout; source it by executing the subshell
     tmpfile="/tmp/dcshell-build${$}"
     $($DCSHELL_HOME/config.py $projdir_opt \
-        -k container_name -k image -k build.context -k build.dockerfile $dc_opt_prefixed) \
-        > $tmpfile
+        -k container_name -k image -k build.context -k build.dockerfile $dc_opt_prefixed) > $tmpfile
     source $tmpfile
     set +e
     rm -f $$tmpfile
 }
-
-
